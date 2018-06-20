f3bffaf67ee8f2759cb76ac0e639e78a0d406847
fenton <jfenton@bookingbug.com>
2018-06-20 17:40:02 +0100

    Update stuff.rb

--- features/step_definitions/stuff.rb
+++ features/step_definitions/stuff.rb
@@ -1,12 +1,5 @@
-Given("I visit youtube") do
- visit('https://www.youtube.com/watch?v=WUa_2lJ6Xks&start_radio=1&list=RDMMWUa_2lJ6Xks')
-end
+Feature: Can I grok it
 
-When("I click {string}") do |string|
-page.body.split('5 years')[1].split
-binding.pry
-end
-
-Then("I get comments") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
+Scenario: Tests that test themselves
+Given I visit localhost
+Then a BookingBug app is testable
